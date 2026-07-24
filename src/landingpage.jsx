1f9628fcc0bc507f9cab56d3d1e7a25961374e8a
pallavithegod <jainpallavi.delhi@gmail.com>
2026-07-24 22:30:48 +0530

    fix navbar

--- src/landingpage.jsx
+++ src/landingpage.jsx
@@ -46,10 +46,10 @@ const xUrl = "https://x.com/coverfidotspace";
 const docsUrl = "https://docs.coverfi.space/";
 const supportEmail = "support@coverfi.space";
 const footerLinks = [
+  { label: "Home", href: "/" },
+  { label: "FAQs", href: "/faqs" },
   ...navItems,
-  { label: "GitHub", href: githubUrl },
-  { label: "X", href: xUrl },
-  { label: "Email", href: `mailto:${supportEmail}` },
+  { label: "Status", href: "/status" },
 ];
 const boxFrameSequence = {
   basePath: "/assets/new-frames-2/ezgif-frame-",
@@ -1263,17 +1263,24 @@ function updateLogosAndLinks() {
   document.querySelectorAll("a[href='/gifting']").forEach((link) => link.setAttribute("href", "/faqs"));
   document.querySelectorAll("a[href='/faq']").forEach((link) => link.setAttribute("href", "/faqs"));
 
-  const socialLinks = [
+  ensureFooterEmailSocialButtons();
+
+  const headerSocialLinks = [
     { icon: "x", href: "https://x.com/coverfidotspace", aria: "Follow CoverFi on X" },
     { icon: "github", href: "https://github.com/coverfi-space", aria: "CoverFi on GitHub" },
   ];
 
-  document
-    .querySelectorAll(
-      ".header__sub .button-social, .menu__sub .button-social, .footer__socials .button-social, .footer__mob-socials .button-social",
-    )
-    .forEach((link, index) => {
-      const social = socialLinks[index % socialLinks.length];
+  const footerSocialLinks = [
+    ...headerSocialLinks,
+    { icon: "email", href: `mailto:${supportEmail}`, aria: "Email CoverFi" },
+  ];
+
+  [
+    [".header__sub .button-social, .menu__sub .button-social", headerSocialLinks],
+    [".footer__socials .button-social, .footer__mob-socials .button-social", footerSocialLinks],
+  ].forEach(([selector, links]) => {
+    document.querySelectorAll(selector).forEach((link, index) => {
+      const social = links[index % links.length];
       link.setAttribute("href", social.href);
       link.setAttribute("aria-label", social.aria);
       link.setAttribute("target", "_blank");
@@ -1283,6 +1290,7 @@ function updateLogosAndLinks() {
       const icon = link.querySelector(".button-social__icon-outer") || link;
       icon.replaceChildren(createSocialIcon(social.icon));
     });
+  });
 }
 
 function createSocialIcon(icon) {
@@ -1293,14 +1301,29 @@ function createSocialIcon(icon) {
   svg.classList.add("coverfi-social-svg");
 
   const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-  path.setAttribute(
-    "d",
+  const pathData =
     icon === "github"
       ? "M12 .5C5.65.5.5 5.65.5 12c0 5.08 3.29 9.39 7.86 10.91.58.11.79-.25.79-.56v-2.17c-3.2.7-3.87-1.36-3.87-1.36-.52-1.33-1.28-1.68-1.28-1.68-1.05-.72.08-.7.08-.7 1.16.08 1.77 1.19 1.77 1.19 1.03 1.76 2.7 1.25 3.36.96.1-.75.4-1.25.73-1.54-2.55-.29-5.23-1.28-5.23-5.69 0-1.26.45-2.28 1.19-3.09-.12-.29-.52-1.46.11-3.04 0 0 .97-.31 3.17 1.18A11 11 0 0 1 12 6.02c.98 0 1.95.13 2.87.39 2.2-1.49 3.17-1.18 3.17-1.18.63 1.58.23 2.75.11 3.04.74.81 1.19 1.83 1.19 3.09 0 4.42-2.69 5.39-5.25 5.68.41.36.78 1.06.78 2.14v3.17c0 .31.21.68.8.56A11.51 11.51 0 0 0 23.5 12C23.5 5.65 18.35.5 12 .5Z"
-      : "M18.9 2h3.35l-7.32 8.36L23.55 22h-6.75l-5.29-6.92L5.46 22H2.1l7.83-8.95L1.67 2h6.92l4.78 6.32L18.9 2Zm-1.18 17.95h1.86L7.58 3.94H5.59l12.13 16.01Z",
-  );
+      : icon === "email"
+        ? "M3.25 5h17.5c.69 0 1.25.56 1.25 1.25v11.5c0 .69-.56 1.25-1.25 1.25H3.25C2.56 19 2 18.44 2 17.75V6.25C2 5.56 2.56 5 3.25 5Zm1.88 2 6.87 5.1L18.87 7H5.13Zm14.87 1.8-7.25 5.38a1.25 1.25 0 0 1-1.5 0L4 8.8V17h16V8.8Z"
+        : "M18.9 2h3.35l-7.32 8.36L23.55 22h-6.75l-5.29-6.92L5.46 22H2.1l7.83-8.95L1.67 2h6.92l4.78 6.32L18.9 2Zm-1.18 17.95h1.86L7.58 3.94H5.59l12.13 16.01Z";
+  path.setAttribute("d", pathData);
   svg.appendChild(path);
   return svg;
+}
+
+function ensureFooterEmailSocialButtons() {
+  document.querySelectorAll(".footer__socials, .footer__mob-socials").forEach((container) => {
+    if (container.querySelector("[data-coverfi-email-social]")) return;
+
+    const template = container.querySelector(".button-social");
+    if (!template) return;
+
+    const emailLink = template.cloneNode(true);
+    emailLink.setAttribute("data-coverfi-email-social", "");
+    emailLink.classList.add("coverfi-email-social");
+    container.appendChild(emailLink);
+  });
 }
 
 function createLogoImage(className) {
